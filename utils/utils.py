c780c1f8af4f3f77df80b41c8d7c656751ebd37b
Ceres6 <cespatorres@gmail.com>
2021-08-07 14:25:44 +0200

    check_list_types added to utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,18 @@
-import matplotlib.pyplot as plt
+from typing import List, Any
 
-def show_image(im, title, *, cmap='gray'):
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def show_image(im: np.ndarray, title: str, *, cmap: str = 'gray') -> None:
     # axes for debugging purposes
     fig, ax = plt.subplots()
     ax.set_title(title)
     ax.imshow(im, cmap=cmap)
-    
+
+
+def check_list_types(checked_list: List, checked_type: Any) -> bool:
+    """Checks if all the elements of a list are of the specified type"""
+    if not isinstance(checked_list, list):
+        return False
+    return all(isinstance(checked_list, checked_type))
