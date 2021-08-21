0f02fc1335c3ad1fcc7fa20f16832a00363bb10a
Ceres6 <cespatorres@gmail.com>
2021-08-21 20:52:44 +0200

    update solver.py to handle incomplete inputs and pass tests

--- solver/solver.py
+++ solver/solver.py
@@ -5,6 +5,13 @@ from latex2sympy2 import latex2sympy
 
 class Solver:
     def __init__(self, latex_eq: str, function: str):
+        if not isinstance(latex_eq, str):
+            raise TypeError("latex_eq arg should be of type string")
+        if not isinstance(function, str):
+            raise TypeError("function arg should be of type string")
+        if not latex_eq:
+            self.latex_solution = ''
+            return
         self.equation = sympy.Eq(*[latex2sympy(eq_side) for eq_side in latex_eq.split('=')])
         logging.debug(self.equation)
         t = sympy.Symbol('t')
@@ -12,9 +19,10 @@ class Solver:
         self.solution = sympy.dsolve(self.equation.subs(function, sympy.Function(function)(t)))
         logging.debug(self.solution)
         self.latex_solution = sympy.latex(self.solution)
-        print(self.latex_solution)
+        logging.debug(self.latex_solution)
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':  # pragma: no cover
     logging.basicConfig(level=logging.DEBUG)
     solver = Solver(r'\frac{d}{dt}y=y', 'y')
 
