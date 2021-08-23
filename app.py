2f2fbe21cdcfcd0c5232242840b32a561f7e6ca5
Ceres6 <cespatorres@gmail.com>
2021-08-23 18:03:44 +0200

    socket io added for equation solving

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_socketio import SocketIO
 from solver.solver import Solver
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins='http://localhost:3000')
 
 
 @app.route("/")
@@ -9,10 +13,15 @@ def index():
 
 
 @app.route("/solver")
-@app.route("/solver/<latex_eq>/<function>")
+@app.route("/solver/<id>/<latex_eq>/<function>")
 def solve_equation(latex_eq='', function=''):
-    return { 'equation': latex_eq, 'solution':Solver(latex_eq, function).latex_solution}
+    return {'equation': latex_eq, 'solution': Solver(latex_eq, function).latex_solution}
+
+
+@socketio.on('equation')
+def solve_equation(data):
+    socketio.emit('solution', Solver(data['equation'], data['function']).latex_solution)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
