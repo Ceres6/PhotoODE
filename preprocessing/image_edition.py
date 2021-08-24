cc019b09e21536ab3ba2c59bc54b863146635d60
Ceres6 <cespatorres@gmail.com>
2021-08-24 21:50:26 +0200

    Image normalization added to dataset creation

--- preprocessing/image_edition.py
+++ preprocessing/image_edition.py
@@ -1,11 +1,14 @@
-import cv2 as cv
+from typing import Iterable, Tuple
+import logging
+import cv2
 import numpy as np
 from scipy import ndimage
 
 from utils.utils import show_image
 
 
-def bounding_square(objs):
+def bounding_square(objs: Iterable[np.ndarray]) -> Tuple[int]:
+    """Returns a single bounding rectangle that contains all input objects"""
     x = min([obj[1].start for obj in objs])
     x_stop = max([obj[1].stop for obj in objs])
     w = x_stop - x
@@ -15,64 +18,60 @@ def bounding_square(objs):
     return x, y, w, h
 
 
-def image_to_square(img, *, pad=[1]*4, debug=False):
-    """Fucntion to broadcast an image into a white-background square"""
+def image_to_square(img: np.ndarray, *, pad: Iterable[int] = [1]*4, debug=False) -> np.ndarray:
+    """Function to broadcast an image into a white-background squared_image"""
     
-    _, img_inv = cv.threshold(img, 80, 255, cv.THRESH_BINARY_INV)
+    _, img_inv = cv2.threshold(img, 0.3, 1, cv2.THRESH_BINARY_INV)
     
     structure = np.ones((3, 3), dtype='uint8')
-    img_labeled, img_ncomponents = ndimage.label(img_inv, structure) 
-    
-    # try:
-    #     assert(img_ncomponents == 1)
-    # except AssertionError:
-    #     print(f"There are too many components: {img_ncomponents}")
-    #     return
-        
+    img_labeled, img_ncomponents = ndimage.label(img_inv, structure)
+
     objs = ndimage.find_objects(img_labeled)
     x, y, w, h = bounding_square(objs)
     
     if w < h:
-        crop_img = cv.copyMakeBorder(img[y:y+h, x:x+w], pad[0], pad[1], 0, 0,
-                                     cv.BORDER_CONSTANT, value=255)
+        crop_img = cv2.copyMakeBorder(img[y:y + h, x:x + w], pad[0], pad[1], 0, 0,
+                                      cv2.BORDER_CONSTANT, value=1)
     elif w > h:
-        crop_img = cv.copyMakeBorder(img[y:y+h, x:x+w], 0, 0, pad[2], pad[3],
-                                     cv.BORDER_CONSTANT, value=255)
+        crop_img = cv2.copyMakeBorder(img[y:y + h, x:x + w], 0, 0, pad[2], pad[3],
+                                      cv2.BORDER_CONSTANT, value=1)
     else:
-        crop_img = cv.copyMakeBorder(img[y:y+h, x:x+w], pad[0], pad[1], pad[2], 
-                                     pad[3], cv.BORDER_CONSTANT, value=255)
+        crop_img = cv2.copyMakeBorder(img[y:y + h, x:x + w], pad[0], pad[1], pad[2],
+                                      pad[3], cv2.BORDER_CONSTANT, value=1)
     height, width = crop_img.shape
     side = max(height, width)
     
-    square = np.ones((side, side), np.uint8) * 255   
+    squared_image = np.ones((side, side), np.uint8) * 255
     try:
-        square[int((side - height) / 2):height + int((side - height) / 2), 
-               int((side - width) / 2):width + int((side - width) / 2)] = crop_img
+        squared_image[int((side - height) / 2):height + int((side - height) / 2),
+                      int((side - width) / 2):width + int((side - width) / 2)] = crop_img
     except ValueError as e:
-        print(f"side: {side}, height: {height}, width: {width}")
-        print(f"{int((side - height) / 2)}:{height + int((side - height) / 2)}")
-        print(f"{int((side - width) / 2)}:{width + int((side - width) / 2)}")
+        logging.error(f"side: {side}, height: {height}, width: {width}")
+        logging.error(f"{int((side - height) / 2)}:{height + int((side - height) / 2)}")
+        logging.error(f"{int((side - width) / 2)}:{width + int((side - width) / 2)}")
         raise e
-    # cv2.imwrite(out_img,square)
     if debug:
         show_image(crop_img, "original")
-        show_image(square, "square")
+        show_image(squared_image, "squared_image")
     
-    return square
+    return squared_image
 
 
-def resize_threshold(image, size, *, debug=False):
-    
-    img = cv.resize(image, size, interpolation=cv.INTER_LANCZOS4)
+def resize_threshold(image: np.ndarray, size: int, *, debug=False) -> np.ndarray:
+    """
+    Returns the image resized to the input size and thresholded to a value with one connected component if possible
+    """
+    norm_image = cv2.normalize(image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    resized_img = cv2.resize(norm_image, size, interpolation=cv2.INTER_LANCZOS4)
  
-    threshold_values = (80, 100, 150, 180, 200, 215, 230)
+    threshold_values = np.arange(0.3, 1, 0.1)
     structure = np.ones((3, 3), dtype='uint8')
     for threshold in threshold_values:
-        _, img_inv = cv.threshold(img, threshold, 255, cv.THRESH_BINARY_INV)   
+        _, img_inv = cv2.threshold(resized_img, threshold, 1, cv2.THRESH_BINARY_INV)
         _, ncomponents = ndimage.label(img_inv, structure)   
         if ncomponents == 1:
             break
     if debug:
-        show_image(img, "resized")
-    _, img = cv.threshold(img, threshold, 255, cv.THRESH_BINARY)
-    return img
+        show_image(resized_img, "resized")
+    _, resized_img = cv2.threshold(resized_img, threshold, 1, cv2.THRESH_BINARY)
+    return resized_img
