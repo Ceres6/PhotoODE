607627f56628db4ebf87dec366d60c95630ed624
Ceres6 <cespatorres@gmail.com>
2021-08-24 17:39:12 +0200

    segmentation_app created as segmentation module entrypoint

---
+++ segmentation_app.py
@@ -0,0 +1,26 @@
+import os
+import json
+from flask import Flask
+from segmentation.xy_segmentation import xy_segmentation
+from kafka.kafka import send_message
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+
+@app.route("/")
+def index():
+    return "Running!"
+
+
+@app.route("/<session_id>", methods=('POST',))
+def segment_image(session_id):
+    segmentation_results, segmentation_structure = xy_segmentation()
+    message = json.dumps({'segmentation_results': [array.tolist() for array in segmentation_results],
+                          'segmentation_structure': segmentation_structure.serialize(),
+                          'session_id': session_id})
+    send_message('segmentation', message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
