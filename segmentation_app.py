e4c8410d65a4598d61780542112bff833f7ea819
Ceres6 <cespatorres@gmail.com>
2021-08-25 13:12:31 +0200

    Update segmentation_app.py

--- segmentation_app.py
+++ segmentation_app.py
@@ -1,13 +1,18 @@
 import os
 import json
-from flask import Flask
-from segmentation.xy_segmentation import xy_segmentation
-from kafka.kafka import send_message
+import logging
 
-from settings import FLASK_SECRET_KEY
+from flask import Flask
+from kafka import kafka
+
+from segmentation.xy_segmentation import xy_segmentation
+from settings import FLASK_SECRET_KEY, LOG_LEVEL
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = FLASK_SECRET_KEY
+
+logging.basicConfig(level=LOG_LEVEL)
+producer = kafka.init_producer()
 
 
 @app.route("/")
@@ -15,14 +20,16 @@ def index():
     return "Running!"
 
 
-@app.route("/<session_id>", methods=('POST',))
+@app.route("/<session_id>", methods=('POST', 'GET'))
 def segment_image(session_id):
-    segmentation_results, segmentation_structure = xy_segmentation()
+    path = 'C:/Users/cespa/Desktop/Programming_languages/PhotoODE/dataset/segmentation/'
+    segmentation_results, segmentation_structure = xy_segmentation(path)
     message = json.dumps({'segmentation_results': [array.tolist() for array in segmentation_results],
                           'segmentation_structure': segmentation_structure.serialize(),
                           'session_id': session_id})
-    send_message('segmentation', message)
+    kafka.send_message(producer, 'segmentation', message)
+    return "{'status': 'sent for processing'}", 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=(LOG_LEVEL == 'DEBUG'))
