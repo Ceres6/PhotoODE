f596099b2f92f706cd915e27efc46615f31f410e
Ceres6 <cespatorres@gmail.com>
2021-08-25 13:12:00 +0200

    kafka updated to separete Consumer and Producer configuration

--- kafka/kafka.py
+++ kafka/kafka.py
@@ -10,17 +10,18 @@ topic_prefix = CLOUDKARAFKA_TOPIC_PREFIX
 
 # Consumer configuration
 # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
-conf = {
+common_conf = {
     'bootstrap.servers': CLOUDKARAFKA_BROKERS,
-    'session.timeout.ms': 6000,
-    'default.topic.config': {'auto.offset.reset': 'smallest'},
     'security.protocol': 'SASL_SSL',
     'sasl.mechanisms': 'SCRAM-SHA-256',
     'sasl.username': CLOUDKARAFKA_USERNAME,
     'sasl.password': CLOUDKARAFKA_PASSWORD
 }
 
-producer = Producer(**conf)
+
+def init_producer():
+    producer = Producer(**common_conf)
+    return producer
 
 
 def delivery_callback(err, msg):
@@ -30,7 +31,7 @@ def delivery_callback(err, msg):
         logging.info(f' Message delivered to {msg.topic()} [{msg.partition()}]\n')
 
 
-def send_message(topic_suffix: str, message):
+def send_message(producer, topic_suffix: str, message):
     try:
         producer.produce(topic_prefix + topic_suffix, message, callback=delivery_callback)
     except BufferError as e:
@@ -42,22 +43,31 @@ def send_message(topic_suffix: str, message):
 
 
 def init_consumer(topic_suffix: str):
+    conf = {
+        **common_conf,
+        'session.timeout.ms': 6000,
+        'default.topic.config': {'auto.offset.reset': 'smallest'},
+        'group.id': 'LeNet'
+    }
     consumer = Consumer(**conf)
     consumer.subscribe([topic_prefix + topic_suffix])
+    return consumer
 
-    while True:
-        msg = consumer.poll(timeout=1.0)
-        if msg is None:
-            continue
-        if msg.error():
-            # Error or event
-            if msg.error().code() == KafkaError._PARTITION_EOF:
-                # End of partition event
-                logging.error(f'{msg.topic()} [{msg.partition()}] reached end at offset {msg.offset()}\n')
-            elif msg.error():
-                # Error
-                raise KafkaException(msg.error())
-        else:
-            # Proper message
-            logging.error(f'{msg.topic()} [{msg.partition()}] at offset {msg.offset()} with key {str(msg.key())}:\n')
-            print(msg.value())
+
+def consumer_cycle(consumer):
+    msg = consumer.poll(timeout=1.0)
+    if msg is None:
+        return
+    if msg.error():
+        # Error or event
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            # End of partition event
+            logging.error(f'{msg.topic()} [{msg.partition()}] reached end at offset {msg.offset()}\n')
+        elif msg.error():
+            # Error
+            raise KafkaException(msg.error())
+    else:
+        # Proper message
+        logging.error(f'{msg.topic()} [{msg.partition()}] at offset {msg.offset()} with key {str(msg.key())}:\n')
+        logging.info(f'received message: {msg.value()}')
+        return msg
