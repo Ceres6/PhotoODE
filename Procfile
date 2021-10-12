6847805ac2e0986a2e06578a338ca3cd22f589a2
Ceres6 <43477095+Ceres6@users.noreply.github.com>
2021-10-12 17:58:14 +0200

    Parser (#1)
    * Create Procfile
    
    * Update Procfile
    
    * cors origins configured
    
    * updated for multithread
    
    * sleep forcing
    
    * debug logging
    
    * Updates to manage threading
    
    * port binding fix
    
    * fix in negative levels
    
    * closing curly brackets bug fixed
    
    * Labels updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python parse_app.py
--- utils/utils.py
+++ utils/utils.py
@@ -1,23 +1,12 @@
 from typing import Any, List
 
-import matplotlib.pyplot as plt
 import numpy as np
-
 
 def check_list_types(checked_list: List, checked_type: Any) -> bool:
     """Checks if all the elements of a list are of the specified type"""
     if not isinstance(checked_list, list):
         return False
     return all(isinstance(checked_list_element, checked_type) for checked_list_element in checked_list)
-
-
-def show_image(im: np.ndarray, title: str, *, cmap: str = 'gray') -> None:
-    """Plots the input image with pyplot"""
-    # axes for debugging purposes
-    fig, ax = plt.subplots()
-    ax.set_title(title)
-    ax.imshow(im, cmap=cmap)
-    plt.show()
 
 
 def normalize_img(image: np.ndarray) -> np.ndarray:
