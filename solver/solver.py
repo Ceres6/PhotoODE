7bd9056ce5e1079b846fa2da5d0c58052ee8e523
Ceres6 <cespatorres@gmail.com>
2021-10-16 19:48:12 +0200

    fix bug in solver

--- solver/solver.py
+++ solver/solver.py
@@ -12,11 +12,13 @@ class Solver:
         if not latex_eq:
             self.latex_solution = ''
             return
+        self.solution: sympy.core.relation.Equality = None
         self.equation = sympy.Eq(*[latex2sympy(eq_side) for eq_side in latex_eq.split('=')])
         logging.debug(self.equation)
         t = sympy.Symbol('t')
 
         self.solution_dict = sympy.dsolve(self.equation.subs(function, sympy.Function(function)(t)), hint='all')
+        self.best_solution()
         logging.debug(self.solution)
         self.latex_solution = sympy.latex(self.solution)
         logging.debug(self.latex_solution)
@@ -24,13 +26,13 @@ class Solver:
     def best_solution(self) -> None:
         best_hint = self.solution_dict.pop('best_hint')
         if best_hint and best_hint != '1st_power_series':
-            return self.solution_dict['best']
+            self.solution = self.solution_dict['best']
+            return
         for key in ['1st_power_series', 'order', 'default']:
             self.solution_dict.pop(key, None)
-        solution = self.solution_dict.pop('best', None)
+        self.solution = self.solution_dict.pop('best', None)
         if len(self.solution_dict):
-            solution = tuple(self.solution_dict.values()[0])
-        return solution
+            self.solution = tuple(self.solution_dict.values()[0])
 
 
 if __name__ == '__main__':  # pragma: no cover
